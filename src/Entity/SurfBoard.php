3189904d34c8420ea5e0d7cc65845298fd5a9891
Karim <kouach2002@hotmail.com>
2022-12-11 19:02:52 +0100

    creation SurfBoard Entity

---
+++ src/Entity/SurfBoard.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SurfBoardRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SurfBoardRepository::class)]
+class SurfBoard
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nickname = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $length = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $width = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $thickness = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $material = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $shaper = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $brand = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $review = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $datePurchase = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $new = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(?string $nickname): self
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    public function getLength(): ?float
+    {
+        return $this->length;
+    }
+
+    public function setLength(?float $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getWidth(): ?float
+    {
+        return $this->width;
+    }
+
+    public function setWidth(?float $width): self
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    public function getThickness(): ?float
+    {
+        return $this->thickness;
+    }
+
+    public function setThickness(?float $thickness): self
+    {
+        $this->thickness = $thickness;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getMaterial(): ?string
+    {
+        return $this->material;
+    }
+
+    public function setMaterial(?string $material): self
+    {
+        $this->material = $material;
+
+        return $this;
+    }
+
+    public function getShaper(): ?string
+    {
+        return $this->shaper;
+    }
+
+    public function setShaper(?string $shaper): self
+    {
+        $this->shaper = $shaper;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getReview(): ?string
+    {
+        return $this->review;
+    }
+
+    public function setReview(?string $review): self
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+
+    public function getDatePurchase(): ?\DateTimeInterface
+    {
+        return $this->datePurchase;
+    }
+
+    public function setDatePurchase(?\DateTimeInterface $datePurchase): self
+    {
+        $this->datePurchase = $datePurchase;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isNew(): ?bool
+    {
+        return $this->new;
+    }
+
+    public function setNew(?bool $new): self
+    {
+        $this->new = $new;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
