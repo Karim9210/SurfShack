e7e65be081dd6cc6285d40e05731dafc145bef58
Karim <kouach2002@hotmail.com>
2022-12-21 01:08:02 +0100

    start css...

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,19 +4,60 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            // ->add('email')
             // ->add('roles')
-            ->add('password')
-            ->add('picture')
-            ->add('pseudo');
+            // ->add('password')
+            // ->add('picture')
+            // ->add('pseudo');
+            ->add('pseudo', TextType::class, [
+                'required'  => true,
+                'label' => false,
+            ])
+
+            ->add('email', EmailType::class, [
+                'required'  => true,
+                'label' => false,
+            ])
+            ->add('pictureFile', VichFileType::class, [
+                'required'      => false,
+                'label' => false,
+                'allow_delete'  => false, // not mandatory, default is true
+                'download_uri' => false, // not mandatory, default is true
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'label' => false,
+
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
